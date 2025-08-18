10007d582dbadd6d4d74a96bdb23881eba79576f
Rafli <raflijf7@gmail.com>
2025-08-18 20:42:49 +0700

    feat: menambahkan title dinamis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import SearchBar from "../components/forms/SearchBar";
 import PrimaryButton from "../components/button/PrimaryButton";
 import PostCard from "../components/post/PostCard";
@@ -8,21 +10,26 @@ import PostCardSkeleton from "../components/loading/PostCardSkeleton";
 export default function Home() {
    
     return (
-        <div className="grid  grid-cols-[1fr_10fr_1fr] md:grid-cols-[minmax(0,1fr)_minmax(0,8fr)_minmax(0,1fr)]  ">
-            <div className="  col-start-2 grid grid-rows-[0fr_1fr] gap-y-10">
-                <div className="grid-rows-[1fr_1fr_0fr] grid place-items-center ">
-                    <img src={logo} alt="" className="w-75"/>         
-                    <SearchBar/>
-                    <PrimaryButton>Buat resep anda sendiri</PrimaryButton>                
+        <>
+            <Helmet>
+                <title>Beranda</title>
+            </Helmet>
+            <div className="grid  grid-cols-[1fr_10fr_1fr] md:grid-cols-[minmax(0,1fr)_minmax(0,8fr)_minmax(0,1fr)]  ">
+                <div className="  col-start-2 grid grid-rows-[0fr_1fr] gap-y-10">
+                    <div className="grid-rows-[1fr_1fr_0fr] grid place-items-center ">
+                        <img src={logo} alt="" className="w-75"/>         
+                        <SearchBar/>
+                        <PrimaryButton>Buat resep anda sendiri</PrimaryButton>                
+                    </div>
+                    <div className="grid  place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 gap-y-14 md">
+                    <PostCard/>
+                    <PostCard/>
+                    <PostCard/>
+                        
+                    </div>
+                
                 </div>
-                <div className="grid  place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 gap-y-14 md">
-                   <PostCard/>
-                   <PostCard/>
-                   <PostCard/>
-                    
-                </div>
-               
             </div>
-        </div>
+        </>
     )
 }
