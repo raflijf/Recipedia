35d341b7fef08bde5977e70724a49447038ab1a1
Rafli <raflijf7@gmail.com>
2025-08-19 19:07:12 +0700

    feat: manambahkan route halaman resep

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import MainLayout from "./layout/MainLayout"
 import Home from "./pages/Home"
+import Recipes from "./pages/Recipes"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout/>} >
           <Route path="/" element={<Home/>} />
+          <Route path="/recipe" element={<Recipes/>} />
         </Route>
       </Routes>
     </Router>
