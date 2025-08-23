fae6b3d67148df8c9fba2649e448d2b8814d458e
Rafli <raflijf7@gmail.com>
2025-08-23 20:27:52 +0700

    mengubah layout  untuk mobile

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,15 +4,15 @@ import Logo from '../assets/Recipedia.png'
 
 export default function Footer() {
     return (
-        <div className="w-full h-auto mt-20 p-5 ">
-            <footer className="bg-thirdty text-white w-full h-full rounded-lg p-4 grid grid-rows-[1fr_auto]  grid-cols-2 sm:grid-cols-3 lg:grid-cols-5   gap-5   ">
+        <div className="w-full h-auto mt-2 lg:p-5  ">
+            <footer className="bg-thirdty text-white w-full h-full md:rounded-lg p-4 grid grid-rows-[1fr_auto]  grid-cols-2 sm:grid-cols-3 lg:grid-cols-5   gap-5   ">
                 <div className='border-white  col-span-full lg:col-span-2'>
                     <img src={Logo} alt="logo" />
                     <p className='text-sm'>Recipedia adalah platform pribadi untuk menyimpan dan mengeksplorasi resep favoritmu. Dibuat sebagai proyek latihan fullstack menggunakan React & Django.</p>
                 </div>
                 <div>
-                    <h2 className='text-xl font-bold mb-4 max-sm:text-center'>Navigation</h2>
-                    <ul className='grid gap-1 max-sm:justify-items-center'>
+                    <h2 className='text-xl font-bold mb-4 '>Navigation</h2>
+                    <ul className='grid gap-1'>
                         <li>
                             <Link className='underline-offset-4 hover:underline' to={'/'}>Beranda</Link>
                         </li>
@@ -33,8 +33,8 @@ export default function Footer() {
                 </div>
                 
                 <div>
-                    <h2 className='text-xl font-bold mb-4 max-sm:text-center'>Resources</h2>
-                    <ul className='grid gap-1 max-sm:justify-items-center '>
+                    <h2 className='text-xl font-bold mb-4 '>Resources</h2>
+                    <ul className='grid gap-1 '>
                         <li className='underline-offset-4 hover:underline cursor-pointer w-fit '>API</li>
                         <li className='underline-offset-4 hover:underline cursor-pointer w-fit'>Docs</li>
                         <li className='underline-offset-4 hover:underline cursor-pointer w-fit'>Tutorial</li>
@@ -42,9 +42,9 @@ export default function Footer() {
                     </ul>
                 </div>
                 
-                <div className='max-sm:col-span-full'>
-                    <h2 className='text-xl font-bold mb-4  max-sm:text-center'>Connect</h2>
-                    <ul className='grid gap-1 max-sm:justify-items-center'>
+                <div>
+                    <h2 className='text-xl font-bold mb-4  '>Connect</h2>
+                    <ul className='grid gap-1'>
                         <li className='underline-offset-4 hover:underline cursor-pointer w-fit '>Github</li>
                         <li className='underline-offset-4 hover:underline cursor-pointer w-fit'>Tiktok</li>
                         <li className='underline-offset-4 hover:underline cursor-pointer w-fit'>Instagram</li>
