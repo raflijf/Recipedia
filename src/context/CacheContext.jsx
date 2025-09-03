264d5cfebfa2db0a43f310a9ee2a8c63e5735a56
Rafli <raflijf7@gmail.com>
2025-09-03 20:07:28 +0700

    perbarui default value datapost state

--- src/context/CacheContext.jsx
+++ src/context/CacheContext.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const CacheContext = createContext([])
 
 function CacheProvider({children}) {
-    const [dataPost, setDataPost] = useState([])
+    const [dataPost, setDataPost] = useState({data : [] , total : 0})
     return (
         <CacheContext.Provider value={{dataPost, setDataPost}}>
             {children}
